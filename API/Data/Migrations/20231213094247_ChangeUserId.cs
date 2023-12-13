b390050de64b07dc117b1722789b513a8d6631a1
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2023-12-13 14:45:00 +0500

    fix: change basket userId to buyerId

---
+++ API/Data/Migrations/20231213094247_ChangeUserId.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Baskets");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BuyerId",
+                table: "Baskets",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuyerId",
+                table: "Baskets");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Baskets",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- API/Entities/Basket.cs
+++ API/Entities/Basket.cs
@@ -3,7 +3,7 @@ namespace API.Entities
     public class Basket
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
+        public string BuyerId { get; set; }
         public List<BasketItem> Items { get; set; } = new();
 
         public void AddItem(Product product, int quantity)
