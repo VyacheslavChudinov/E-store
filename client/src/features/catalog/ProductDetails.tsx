eab10ebd514d4e2ada75bf242dd519e15c1d3b62
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2023-12-14 23:32:04 +0500

    feat: add logic to add products and change product quantity on product details page

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -6,6 +6,7 @@ import {
   TableCell,
   TableContainer,
   TableRow,
+  TextField,
   Typography,
 } from "@mui/material";
 import { useEffect, useState } from "react";
@@ -14,19 +15,63 @@ import { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
 import { NotFound } from "../errors/NotFound";
 import Loading from "../../app/layouts/Loading";
+import { useStoreContext } from "../../app/context/StoreContext";
+import { LoadingButton } from "@mui/lab";
 
 export default function ProductDetails() {
+  const { basket, setBasket, removeItem } = useStoreContext();
   const { id } = useParams<{ id: string }>();
   const [product, setProduct] = useState<Product | null>(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+  const item = basket?.items.find(
+    (basketItem) => basketItem.productId === product?.id
+  );
+
+  function onQuantityChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = parseInt(event.target.value);
+    if (value >= 0) {
+      setQuantity(value);
+    }
+  }
+
+  function onSubmit() {
+    if (!product) {
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    if (!item || quantity > item.quantity) {
+      const quantityToAdd = quantity - (item?.quantity ?? 0);
+      agent.Basket.addItem(product?.id, quantityToAdd)
+        .then((basket) => setBasket(basket))
+        .catch((error) => console.log(error))
+        .finally(() => setIsSubmitting(false));
+
+      return;
+    }
+
+    if (quantity < item.quantity) {
+      const quantityToRemove = item.quantity - quantity;
+      agent.Basket.remove(item?.productId, quantityToRemove)
+        .then(() => removeItem(item?.productId, quantityToRemove))
+        .catch((error) => console.log(error))
+        .finally(() => setIsSubmitting(false));
+    }
+  }
 
   useEffect(() => {
-    id &&
-      agent.Catalog.getProduct(parseInt(id))
-        .then((response) => setProduct(response))
-        .catch((error) => console.log(error))
-        .finally(() => setIsLoading(false));
-  }, [id]);
+    if (!id) {
+      return;
+    }
+
+    agent.Catalog.getProduct(parseInt(id))
+      .then((response) => setProduct(response))
+      .catch((error) => console.log(error))
+      .finally(() => setIsLoading(false));
+  }, [id, item]);
 
   if (isLoading) return <Loading message="Loading Product..." />;
   if (!product) return <NotFound />;
@@ -72,6 +117,34 @@ export default function ProductDetails() {
             </TableBody>
           </Table>
         </TableContainer>
+        <Grid container spacing={2} sx={{ mt: "10px" }}>
+          <Grid item xs={6}>
+            <TextField
+              variant="outlined"
+              type="number"
+              label="Quantity in cart"
+              fullWidth
+              value={quantity}
+              onChange={onQuantityChange}
+            ></TextField>
+          </Grid>
+          <Grid item xs={6}>
+            <LoadingButton
+              loading={isSubmitting}
+              sx={{ height: "55px" }}
+              color="primary"
+              size="large"
+              variant="contained"
+              fullWidth
+              onClick={onSubmit}
+              disabled={
+                item?.quantity === quantity || (!item && quantity === 0)
+              }
+            >
+              {item ? "Update quantity" : "Add to cart"}
+            </LoadingButton>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
