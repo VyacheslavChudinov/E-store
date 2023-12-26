3aabbf0a86da893bf809b85e94b67ebef2eeb1dc
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2023-12-26 21:43:16 +0500

    fix: use react form context to save form state between steps

--- client/src/features/checkout/AddressForm.tsx
+++ client/src/features/checkout/AddressForm.tsx
@@ -1,9 +1,9 @@
 import { Typography, Grid, FormControlLabel, Checkbox } from "@mui/material";
 import StoreFormInput from "../../app/components/StoreFormInput";
-import { useForm } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 
 export default function AddressForm() {
-  const { control, handleSubmit } = useForm();
+  const { control } = useFormContext();
 
   return (
     <>
@@ -11,52 +11,49 @@ export default function AddressForm() {
         Shipping address
       </Typography>
 
-      <form onSubmit={handleSubmit(console.log)}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>
-            <StoreFormInput control={control} name="firstName" label="Name" />
-          </Grid>
-          <Grid item xs={12}>
-            <StoreFormInput
-              control={control}
-              name="address1"
-              label="Address line 1"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <StoreFormInput
-              control={control}
-              name="address2"
-              label="Address line 2"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <StoreFormInput control={control} name="city" label="City" />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <StoreFormInput control={control} name="state" label="State" />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <StoreFormInput
-              control={control}
-              name="zip"
-              label="Zip / Postal code"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <StoreFormInput control={control} name="country" label="Country" />
-          </Grid>
-          <Grid item xs={12}>
-            <FormControlLabel
-              control={
-                <Checkbox color="secondary" name="saveAddress" value="yes" />
-              }
-              label="Use this address for payment details"
-            />
-          </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <StoreFormInput control={control} name="firstName" label="Name" />
         </Grid>
-        <button type="submit">submit</button>
-      </form>
+        <Grid item xs={12}>
+          <StoreFormInput
+            control={control}
+            name="address1"
+            label="Address line 1"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <StoreFormInput
+            control={control}
+            name="address2"
+            label="Address line 2"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <StoreFormInput control={control} name="city" label="City" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <StoreFormInput control={control} name="state" label="State" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <StoreFormInput
+            control={control}
+            name="zip"
+            label="Zip / Postal code"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <StoreFormInput control={control} name="country" label="Country" />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={
+              <Checkbox color="secondary" name="saveAddress" value="yes" />
+            }
+            label="Use this address for payment details"
+          />
+        </Grid>
+      </Grid>
     </>
   );
 }
