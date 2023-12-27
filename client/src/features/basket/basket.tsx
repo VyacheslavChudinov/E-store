736b674bbde2fbc6d217c2f469a36eb19d3a9bc2
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2023-12-27 18:42:55 +0500

    chore: reuse basket view on basket and checkout pages

--- client/src/features/basket/basket.tsx
+++ client/src/features/basket/basket.tsx
@@ -1,135 +1,19 @@
 import { Basket } from "../../app/models/basket";
-import {
-  Box,
-  Button,
-  Grid,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from "@mui/material";
-import { Add, Delete, Remove } from "@mui/icons-material";
-import { LoadingButton } from "@mui/lab";
+import { Button, Grid, Typography } from "@mui/material";
 import BasketSummary from "./BasketSummary";
-import { formatPrice } from "../../app/utils/format";
 import { Link } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { addBasketItemAsync, removeBasketItemAsync } from "./basketSlice";
+import { useAppSelector } from "../../app/store/configureStore";
+import { BasketTable } from "./BasketTable";
 
 export default function Basket() {
-  const dispatch = useAppDispatch();
-
   const { basket } = useAppSelector((state) => state.basket);
-
-  const { updatingProducts } = useAppSelector((state) => state.basket);
-
-  async function onRemove(productId: number, quantity: number = 1) {
-    await dispatch(
-      removeBasketItemAsync({
-        productId,
-        quantity,
-      })
-    );
-  }
 
   if (!basket)
     return <Typography variant="h3">Your basket is empty</Typography>;
 
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="Basket">
-          <TableHead>
-            <TableRow>
-              <TableCell></TableCell>
-              <TableCell align="left">Name</TableCell>
-              <TableCell align="left">Brand</TableCell>
-              <TableCell align="left">Type</TableCell>
-              <TableCell align="center">Quantity</TableCell>
-              <TableCell align="right">Price</TableCell>
-              <TableCell align="right">Subtotal</TableCell>
-              <TableCell align="center"></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {basket.items.map((item) => (
-              <TableRow
-                key={item.productId}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  <Box
-                    component="img"
-                    sx={{
-                      maxHeight: { xs: 70, md: 100 },
-                    }}
-                    alt="Product image"
-                    src={item.pictureUrl}
-                  />
-                </TableCell>
-                <TableCell align="left" component="th" scope="row">
-                  {item.name}
-                </TableCell>
-                <TableCell align="left">{item.brand}</TableCell>
-                <TableCell align="left">{item.type}</TableCell>
-                <TableCell align="center">
-                  <LoadingButton
-                    loading={
-                      !!updatingProducts.find(
-                        (updatingProductsId) =>
-                          updatingProductsId === item.productId
-                      )
-                    }
-                    onClick={() => onRemove(item.productId)}
-                  >
-                    <Remove />
-                  </LoadingButton>
-
-                  {item.quantity}
-
-                  <LoadingButton
-                    loading={
-                      !!updatingProducts.find(
-                        (updatingProductsId) =>
-                          updatingProductsId === item.productId
-                      )
-                    }
-                    onClick={() =>
-                      dispatch(
-                        addBasketItemAsync({ productId: item.productId })
-                      )
-                    }
-                  >
-                    <Add />
-                  </LoadingButton>
-                </TableCell>
-                <TableCell align="right">{formatPrice(item.price)}</TableCell>
-                <TableCell align="right">
-                  {formatPrice(item.price * item.quantity)}
-                </TableCell>
-                <TableCell align="center">
-                  <LoadingButton
-                    loading={
-                      !!updatingProducts.find(
-                        (updatingProductsId) =>
-                          updatingProductsId === item.productId
-                      )
-                    }
-                    onClick={() => onRemove(item.productId, item.quantity)}
-                    sx={{ color: "red" }}
-                  >
-                    <Delete />
-                  </LoadingButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <BasketTable isBasket={true} items={basket.items} />
 
       <Grid container>
         <Grid item xs={6}></Grid>
