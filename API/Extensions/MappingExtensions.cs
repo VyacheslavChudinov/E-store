b6ce6216926d2a442be816e10ea6b1718258b5fb
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-01-09 22:30:10 +0500

    chore: use automapper to map to order dto

--- API/Extensions/MappingExtensions.cs
+++ API/Extensions/MappingExtensions.cs
@@ -1,6 +1,8 @@
 using API.DTOs;
 using API.Entities;
+using API.Entities.OrderAggregator;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 
 namespace API.Extensions
 {
@@ -16,6 +18,16 @@ namespace API.Extensions
         public static BasketDto MapTo<BasketDto>(this Basket basket)
         {
             return _mapper.Map<BasketDto>(basket);
+        }
+
+        public static OrderDto MapTo<OrderDto>(this Order order)
+        {
+            return _mapper.Map<OrderDto>(order);
+        }
+
+        public static IQueryable<OrderDto> MapToOrderDto(this IQueryable<Order> query)
+        {
+            return query.ProjectTo<OrderDto>(_mapper.ConfigurationProvider);
         }
     }
 }
--- API/RequestHelpers/MappingProfiles.cs
+++ API/RequestHelpers/MappingProfiles.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using API.Entities.OrderAggregator;
 using AutoMapper;
 
 namespace API.RequestHelpers
@@ -19,6 +20,18 @@ namespace API.RequestHelpers
                 .ForMember(dest => dest.Brand, opt => opt.MapFrom(src => src.Product.Brand))
                 .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Product.Type))
                 .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Product.Price));
+
+            CreateMap<Order, OrderDto>()
+                    .ForMember(dest => dest.OrderStatus, opt => opt.MapFrom(src => src.OrderStatus.ToString()))
+                    .ForMember(dest => dest.Total, opt => opt.MapFrom(src => src.GetTotal()))
+                    .ForMember(dest => dest.OrderItems, opt => opt.MapFrom(src => src.OrderItems));
+
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.OrderedItemSnapshot.ProductId))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.OrderedItemSnapshot.Name))
+                .ForMember(dest => dest.PictureUrl, opt => opt.MapFrom(src => src.OrderedItemSnapshot.PictureUrl))
+                .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price))
+                .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity));
         }
     }
 }
