ff4cbcc1b6cd3ebdab4f751ad706b7b1519dfc98
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-01-12 21:28:30 +0500

    chore: update basket slice to use latest redux toolkit syntax for thunks

--- client/src/features/basket/basketSlice.ts
+++ client/src/features/basket/basketSlice.ts
@@ -1,4 +1,8 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  PayloadAction,
+  asyncThunkCreator,
+  buildCreateSlice,
+} from "@reduxjs/toolkit";
 import { Basket } from "../../app/models/basket";
 import agent from "../../app/api/agent";
 import { getCookie } from "../../app/utils/cookie";
@@ -15,124 +19,137 @@ const initialState: BasketState = {
   updatingProducts: [],
 };
 
-export const fetchCurrentBasket = createAsyncThunk(
-  "basket/fetchCurrentBasket",
-  async (_, thunkAPI) => {
-    try {
-      return await agent.Basket.get();
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      thunkAPI.rejectWithValue({ error: error.data });
-    }
-  },
-  {
-    condition: () => !!getCookie("buyerId"),
-  }
-);
-
-export const addBasketItemAsync = createAsyncThunk<
-  Basket,
-  { productId: number; quantity?: number }
->(
-  "basket/addBasketItemAsync",
-  async ({ productId, quantity = 1 }, thunkAPI) => {
-    try {
-      return await agent.Basket.addItem(productId, quantity);
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      return thunkAPI.rejectWithValue(error.data);
-    }
-  }
-);
-
-export const removeBasketItemAsync = createAsyncThunk<
-  void,
-  { productId: number; quantity: number }
->("basket/removeBasketItemAsync", async ({ productId, quantity }, thunkAPI) => {
-  try {
-    await agent.Basket.remove(productId, quantity);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error: any) {
-    return thunkAPI.rejectWithValue(error.data);
-  }
+const createSliceWithThunk = buildCreateSlice({
+  creators: { asyncThunk: asyncThunkCreator },
 });
 
-export const basketSlice = createSlice({
+export const basketSlice = createSliceWithThunk({
   name: "basket",
   initialState,
-  reducers: {
-    setBasket: (state, action) => {
+  reducers: (create) => ({
+    setBasket: create.reducer((state, action: PayloadAction<Basket>) => {
       state.basket = action.payload;
-    },
-    clearBasket: (state) => {
+    }),
+
+    clearBasket: create.reducer((state) => {
       state.basket = null;
-    },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(fetchCurrentBasket.pending, (state) => {
-      state.status = "pendingBasket";
-    });
-    builder.addCase(fetchCurrentBasket.fulfilled, (state, action) => {
-      state.basket = action.payload;
+    }),
 
-      state.status = "idle";
-    });
-    builder.addCase(fetchCurrentBasket.rejected, (state) => {
-      state.status = "idle";
-    });
+    fetchCurrentBasket: create.asyncThunk(
+      async (_, thunkAPI) => {
+        try {
+          return await agent.Basket.get();
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (error: any) {
+          thunkAPI.rejectWithValue({ error: error.data });
+        }
+      },
+      {
+        pending: (state) => {
+          state.status = "pendingBasket";
+        },
+        fulfilled: (state, action) => {
+          state.basket = action.payload;
 
-    builder.addCase(addBasketItemAsync.pending, (state, action) => {
-      console.log(action);
-      state.updatingProducts.push(action.meta.arg.productId);
-    });
-    builder.addCase(addBasketItemAsync.fulfilled, (state, action) => {
-      state.basket = action.payload;
-      state.updatingProducts = state.updatingProducts.filter(
-        (updatingProductId) => updatingProductId !== action.meta.arg.productId
-      );
-    });
-    builder.addCase(addBasketItemAsync.rejected, (state, action) => {
-      console.log(action);
-      state.updatingProducts = state.updatingProducts.filter(
-        (updatingProductId) => updatingProductId !== action.meta.arg.productId
-      );
-    });
-
-    builder.addCase(removeBasketItemAsync.pending, (state, action) => {
-      console.log(action);
-      state.updatingProducts.push(action.meta.arg.productId);
-    });
-    builder.addCase(removeBasketItemAsync.fulfilled, (state, action) => {
-      const { productId, quantity } = action.meta.arg;
-
-      if (!state.basket) {
-        return;
+          state.status = "idle";
+        },
+        rejected: (state) => {
+          state.status = "idle";
+        },
+        options: { condition: () => !!getCookie("buyerId") },
       }
+    ),
 
-      const itemIndex = state.basket.items.findIndex(
-        (item) => item.productId === productId
-      );
+    addBasketItemAsync: create.asyncThunk(
+      async ({ productId, quantity = 1 }, thunkAPI) => {
+        try {
+          return await agent.Basket.addItem(productId, quantity);
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (error: any) {
+          return thunkAPI.rejectWithValue(error.data);
+        }
+      },
+      {
+        pending: (state, action) => {
+          console.log(action);
+          state.updatingProducts.push(action.meta.arg.productId);
+        },
 
-      if (itemIndex === -1) {
-        return;
+        rejected: (state, action) => {
+          console.log(action);
+          state.updatingProducts = state.updatingProducts.filter(
+            (updatingProductId) =>
+              updatingProductId !== action.meta.arg.productId
+          );
+        },
+
+        fulfilled: (state, action) => {
+          state.basket = action.payload;
+          state.updatingProducts = state.updatingProducts.filter(
+            (updatingProductId) =>
+              updatingProductId !== action.meta.arg.productId
+          );
+        },
       }
+    ),
 
-      state.basket.items[itemIndex].quantity -= quantity;
-      if (state.basket.items[itemIndex].quantity <= 0) {
-        state.basket.items.splice(itemIndex, 1);
+    removeBasketItemAsync: create.asyncThunk(
+      async ({ productId, quantity }, thunkAPI) => {
+        try {
+          await agent.Basket.remove(productId, quantity);
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (error: any) {
+          return thunkAPI.rejectWithValue(error.data);
+        }
+      },
+      {
+        pending: (state, action) => {
+          console.log(action);
+          state.updatingProducts.push(action.meta.arg.productId);
+        },
+
+        fulfilled: (state, action) => {
+          const { productId, quantity } = action.meta.arg;
+
+          if (!state.basket) {
+            return;
+          }
+
+          const itemIndex = state.basket.items.findIndex(
+            (item) => item.productId === productId
+          );
+
+          if (itemIndex === -1) {
+            return;
+          }
+
+          state.basket.items[itemIndex].quantity -= quantity;
+          if (state.basket.items[itemIndex].quantity <= 0) {
+            state.basket.items.splice(itemIndex, 1);
+          }
+
+          state.updatingProducts = state.updatingProducts.filter(
+            (updatingProductId) =>
+              updatingProductId !== action.meta.arg.productId
+          );
+        },
+
+        rejected: (state, action) => {
+          console.log(action);
+          state.updatingProducts = state.updatingProducts.filter(
+            (updatingProductId) =>
+              updatingProductId !== action.meta.arg.productId
+          );
+        },
       }
-
-      state.updatingProducts = state.updatingProducts.filter(
-        (updatingProductId) => updatingProductId !== action.meta.arg.productId
-      );
-    });
-    builder.addCase(removeBasketItemAsync.rejected, (state, action) => {
-      console.log(action);
-      state.updatingProducts = state.updatingProducts.filter(
-        (updatingProductId) => updatingProductId !== action.meta.arg.productId
-      );
-    });
-  },
+    ),
+  }),
 });
 
-export const { setBasket, clearBasket } = basketSlice.actions;
+export const {
+  setBasket,
+  clearBasket,
+  fetchCurrentBasket,
+  addBasketItemAsync,
+  removeBasketItemAsync,
+} = basketSlice.actions;
