17006c0a591fe920dad236f8ce226b23b1fdb128
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-01-13 20:01:03 +0500

    feat: add product form to add and update inventory products

---
+++ client/src/features/admin/ProductForm.tsx
@@ -0,0 +1,54 @@
+import { Typography, Grid, Paper, Box, Button } from "@mui/material";
+import { useForm } from "react-hook-form";
+import StoreFormInput from "../../app/components/StoreFormInput";
+
+export default function ProductForm() {
+  const { control } = useForm();
+
+  return (
+    <Box component={Paper} sx={{ p: 4 }}>
+      <Typography variant="h4" gutterBottom sx={{ mb: 4 }}>
+        Product Details
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12}>
+          <StoreFormInput control={control} name="name" label="Product name" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <StoreFormInput control={control} name="brand" label="Brand" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <StoreFormInput control={control} name="type" label="Type" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <StoreFormInput control={control} name="price" label="Price" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <StoreFormInput
+            control={control}
+            name="quantityInStock"
+            label="Quantity in Stock"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <StoreFormInput
+            control={control}
+            name="description"
+            label="Description"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <StoreFormInput control={control} name="pictureUrl" label="Image" />
+        </Grid>
+      </Grid>
+      <Box display="flex" justifyContent="space-between" sx={{ mt: 3 }}>
+        <Button variant="contained" color="inherit">
+          Cancel
+        </Button>
+        <Button variant="contained" color="success">
+          Submit
+        </Button>
+      </Box>
+    </Box>
+  );
+}
