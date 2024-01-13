11a8943e4a93f90e5c59fbd91e3364bcfd62c278
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-01-13 16:19:27 +0500

    feat: use image service to handle product image when updating and removing products

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,97 +9,120 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+public class ProductsController(StoreContext context, IMapper mapper, ImageService imageService) : BaseApiController
 {
-
-    public class ProductsController(StoreContext context, IMapper mapper, ImageService imageService) : BaseApiController
+    [HttpGet]
+    public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery] ProductParams productParams)
     {
-        [HttpGet]
-        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery] ProductParams productParams)
+        var query = context.Products
+            .Sort(productParams.OrderBy)
+            .Search(productParams.SearchTerm)
+            .Filter(productParams.Brands, productParams.Types)
+            .AsQueryable();
+        var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
+
+        Response.AddPaginationHeader(products.PaginationDetails);
+
+        return products;
+    }
+
+    [HttpGet("{id}", Name = "GetProduct")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await context.Products.FindAsync(id);
+
+        if (product == null) return NotFound();
+
+        return product;
+    }
+
+    [HttpGet("filters")]
+    public async Task<IActionResult> GetFilters()
+    {
+        var brands = await context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+        var types = await context.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+
+        return Ok(new { brands, types });
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct([FromForm] CreateProductDto createProductDto)
+    {
+        var product = mapper.Map<Product>(createProductDto);
+
+        if (createProductDto.PictureFile is not null)
         {
-            var query = context.Products
-                .Sort(productParams.OrderBy)
-                .Search(productParams.SearchTerm)
-                .Filter(productParams.Brands, productParams.Types)
-                .AsQueryable();
-            var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
-
-            Response.AddPaginationHeader(products.PaginationDetails);
-
-            return products;
-        }
-
-        [HttpGet("{id}", Name = "GetProduct")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
-        {
-            var product = await context.Products.FindAsync(id);
-
-            if (product == null) return NotFound();
-
-            return product;
-        }
-
-        [HttpGet("filters")]
-        public async Task<IActionResult> GetFilters()
-        {
-            var brands = await context.Products.Select(p => p.Brand).Distinct().ToListAsync();
-            var types = await context.Products.Select(p => p.Type).Distinct().ToListAsync();
-
-
-            return Ok(new { brands, types });
-        }
-
-        [Authorize(Roles = "Admin")]
-        [HttpPost]
-        public async Task<ActionResult<Product>> CreateProduct([FromForm]CreateProductDto createProductDto)
-        {
-            var product = mapper.Map<Product>(createProductDto);
-
-            if (createProductDto.PictureFile is not null)
+            var imageResult = await imageService.AddImageAsync(createProductDto.PictureFile);
+            if (imageResult.Error is not null)
             {
-                var imageResult = await imageService.AddImageAsync(createProductDto.PictureFile);
-                if (imageResult.Error is not null) { return BadRequest(new ProblemDetails { Title = imageResult.Error.Message }); }
-
-                product.PictureUrl = imageResult.SecureUrl.ToString();
-                product.PublicId = imageResult.PublicId;
+                return BadRequest(new ProblemDetails { Title = imageResult.Error.Message });
             }
-            
-            context.Products.Add(product);
 
-            var result = await context.SaveChangesAsync() > 0;
-            if (result) { return CreatedAtRoute("GetProduct", new { product.Id }, product); }
-
-            return BadRequest(new ProblemDetails { Title = "Problem creating new product" });
+            product.PictureUrl = imageResult.SecureUrl.ToString();
+            product.PublicId = imageResult.PublicId;
         }
 
-        [Authorize(Roles = "Admin")]
-        [HttpPut]
-        public async Task<ActionResult> UpdateProduct(UpdateProductDto updateProductDto)
+        context.Products.Add(product);
+
+        var result = await context.SaveChangesAsync() > 0;
+        if (result) return CreatedAtRoute("GetProduct", new { product.Id }, product);
+
+        return BadRequest(new ProblemDetails { Title = "Problem creating new product" });
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPut]
+    public async Task<ActionResult> UpdateProduct([FromForm] UpdateProductDto updateProductDto)
+    {
+        var product = await context.Products.FindAsync(updateProductDto.Id);
+        if (product is null) return NotFound();
+
+        if (updateProductDto.PictureFile is not null)
         {
-            var product = await context.Products.FindAsync(updateProductDto.Id);
-            if (product is null) { return NotFound(); }
+            var imageResult = await imageService.AddImageAsync(updateProductDto.PictureFile);
+            if (imageResult.Error is not null)
+            {
+                return BadRequest(new ProblemDetails { Title = imageResult.Error.Message });
+            }
 
-            mapper.Map(updateProductDto, product);
+            if (!string.IsNullOrEmpty(product.PublicId))
+            {
+                await imageService.DeleteImageAsync(product.PublicId);
+            }
 
-            var result = await context.SaveChangesAsync() > 0;
-            if (!result) { return BadRequest(new ProblemDetails { Title = "Problem updating product." }); }
-
-            return NoContent();
+            product.PictureUrl = imageResult.SecureUrl.ToString();
+            product.PublicId = imageResult.PublicId;
         }
 
-        [Authorize(Roles = "Admin")]
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> DeleteProduct(int id)
+        mapper.Map(updateProductDto, product);
+
+        var result = await context.SaveChangesAsync() > 0;
+        if (!result) return BadRequest(new ProblemDetails { Title = "Problem updating product." });
+
+        return NoContent();
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
+        var product = await context.Products.FindAsync(id);
+        if (product is null) return NotFound();
+
+        if (!string.IsNullOrEmpty(product.PublicId))
         {
-            var product = await context.Products.FindAsync(id);
-            if (product is null) { return NotFound(); }
-
-            context.Products.Remove(product);
-
-            var result = await context.SaveChangesAsync() > 0;
-            if (!result) { return BadRequest(new ProblemDetails { Title = "Problem deleting product." }); }
-
-            return Ok();
+            await imageService.DeleteImageAsync(product.PublicId);
         }
+
+        context.Products.Remove(product);
+
+        var result = await context.SaveChangesAsync() > 0;
+        if (!result) return BadRequest(new ProblemDetails { Title = "Problem deleting product." });
+
+        return Ok();
     }
 }
--- API/DTOs/UpdateProductDto.cs
+++ API/DTOs/UpdateProductDto.cs
@@ -1,30 +1,22 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace API.DTOs
+namespace API.DTOs;
+
+public class UpdateProductDto
 {
-    public class UpdateProductDto
-    {
-        public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
+    public int Id { get; set; }
 
-        [Required]
-        public string Description { get; set; }
+    [Required] public string Name { get; set; }
 
-        [Required]
-        [Range(1, double.MaxValue)]
-        public long Price { get; set; }
+    [Required] public string Description { get; set; }
 
-        public IFormFile PictureUrl { get; set; }
+    [Required] [Range(1, double.MaxValue)] public long Price { get; set; }
 
-        [Required]
-        public string Type { get; set; }
+    public IFormFile PictureFile { get; set; }
 
-        [Required]
-        public string Brand { get; set; }
+    [Required] public string Type { get; set; }
 
-        [Required]
-        [Range(0, 9999)]
-        public int QuantityInStock { get; set; }
-    }
+    [Required] public string Brand { get; set; }
+
+    [Required] [Range(0, 9999)] public int QuantityInStock { get; set; }
 }
