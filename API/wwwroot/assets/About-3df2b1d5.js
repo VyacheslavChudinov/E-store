dbdda5d13cec1f7f040f57c2cde7fa6a646e062a
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-01-18 16:16:29 +0500

    feat: new client build

---
+++ API/wwwroot/assets/About-3df2b1d5.js
@@ -0,0 +1,1 @@
+import{j as t,T as r}from"./index-156a389b.js";function o(){return t.jsx(r,{variant:"h2",children:"About Page"})}export{o as default};
---
+++ API/wwwroot/assets/Contact-b4cbbb1e.js
@@ -0,0 +1,1 @@
+import{j as t,T as a}from"./index-156a389b.js";function n(){return t.jsx(a,{variant:"h2",children:"Contact Page"})}export{n as default};
